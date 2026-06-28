2cddd44fca6d57edc6d0fdc60ff91cdfd65b00b2
Hiroaki-K4 <49no19@gmail.com>
2026-06-28 15:22:23 +0900

    Change gradient_accumulation_steps

--- train_libero.py
+++ train_libero.py
@@ -209,7 +209,7 @@ if __name__ == "__main__":
         num_workers=4,
         device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),
         save_model_path="checkpoints/libero_spatial",
-        gradient_accumulation_steps=2,
+        gradient_accumulation_steps=8,
         lora_r=32,
         lora_alpha=128,
         val_demos=5,
